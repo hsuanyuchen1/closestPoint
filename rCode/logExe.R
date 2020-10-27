cd04f6462d5ddf582f08e8727fb01a1d2744d0dd
hsuanyuchen1 <40208349+hsuanyuchen1@users.noreply.github.com>
2020-10-27 16:02:13 +0800

    Update logExe.R

--- rCode/logExe.R
+++ rCode/logExe.R
@@ -1,17 +1,17 @@
-source("c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/rCode/mainMinDist.R")
+source("mainMinDist.R")
 
-sourceDataDir <- "c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/tw700.csv"
+sourceDataDir <- "tw700.csv"
 
-targetDir <- "c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/test/L700/"
-tempDir <- "c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/test/pending/"
-outDir <- "c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/test/output/"
+targetDir <- "closestPoint/test/L700/"
+tempDir <- "closestPoint/test/pending/"
+outDir <- "closestPoint/test/output/"
 
-successDir <- "c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/test/success/"
-failDir <- "c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/test/fail/"
+successDir <- "closestPoint/test/success/"
+failDir <- "closestPoint/test/fail/"
 
 
 capture.output(mainMinDist(sourceDataDir, targetDir, tempDir, outDir, successDir, failDir),
-               file = "c:/Work/Operators/TWM/MDT/phase2_itmes/closestPoint/log.txt", append = T)
+               file = "closestPoint/log.txt", append = T)
 
 
 
